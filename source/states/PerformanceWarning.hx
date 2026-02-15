c32027a7b5908e20edb64b50d05b1bab3f8368b6
MrMadera <ismaelglhs@gmail.com>
2026-02-15 15:23:40 +0000

    performance overhaul started (not finished yet i wanna play remnants mod lmao)

--- source/states/PerformanceWarning.hx
+++ source/states/PerformanceWarning.hx
@@ -2,6 +2,9 @@ package states;
 
 import states.gallery.GalleryPreload;
 import flixel.FlxSubState;
+import flixel.util.FlxGradient;
+import flixel.addons.display.FlxBackdrop;
+import objects.ParticleGroup;
 
 import flixel.effects.FlxFlicker;
 import lime.app.Application;
@@ -11,8 +14,13 @@ class PerformanceWarning extends MusicBeatState
 	public static var leftState:Bool = false;
 
 	var bg:FlxSprite;
-	var warnText:FlxText;
-	var iUnderstand:FlxText;
+	var icons:FlxBackdrop;
+	var particles:ParticleGroup;
+	var gradient:FlxSprite;
+	var gradient2:FlxSprite;
+	var warningText:Alphabet;
+	var infoText:FlxText;
+	var pressEnterToContinueText:Alphabet;
 
 	override function create()
 	{
@@ -25,30 +33,56 @@ class PerformanceWarning extends MusicBeatState
 		bg = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, 0xFF030007);
 		add(bg);
 
-		warnText = new FlxText(0, 0, FlxG.width,
-			"Warning!\n
-			This Mod may have memory leaks and severe lags if you have a low-end pc\n
-			We recommend you to activate the low quality setting in the options menu to avoid them");
-		warnText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER);
-		warnText.screenCenter(Y);
-		warnText.antialiasing = ClientPrefs.data.antialiasing;
-		warnText.alpha = 0;
-		warnText.y += 10;
-		add(warnText);
+		icons = new FlxBackdrop(Paths.image('mainmenu/icons'), XY);
+		icons.velocity.set(10, 10);
+		icons.alpha = 1;
+		icons.antialiasing = ClientPrefs.data.antialiasing;
+		icons.blend = MULTIPLY;
+		add(icons);
 
-		FlxTween.tween(warnText, {alpha: 1, y: warnText.y - 10}, 0.2);
+		particles = new ParticleGroup(0, 0, FlxG.width, 400, 'particle');
+		//particles.y = FlxG.height - particles.height;
+		particles.startY = FlxG.height;
+		particles.endY = FlxG.height - 450;
+		particles.initialScale = 0.25;
+		particles.fadeSpeed = 0.8;
+		particles.randomX = 90;
+		particles.randomY = 90;
+		add(particles);
 
-		iUnderstand = new FlxText(0, 0, FlxG.width, 'I understand');
-		iUnderstand.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER);
-		iUnderstand.y = (warnText.y + warnText.height) + 44;
-		//iUnderstand.x += (128 * i) - 80;
-        iUnderstand.screenCenter(X);
-		iUnderstand.y += 10;
-		iUnderstand.alpha = 0;
-		iUnderstand.antialiasing = ClientPrefs.data.antialiasing;
-		add(iUnderstand);
+		gradient = FlxGradient.createGradientFlxSprite(FlxG.width, FlxG.height, ([0xFF030007, 0xFF4B2E71]));
+		//blackScreenGradient.alpha = 0.45;
+		gradient.alpha = 0.45;
+		add(gradient);
 
-		FlxTween.tween(iUnderstand, {alpha: 1, y: iUnderstand.y - 10}, 0.2);
+		gradient2 = FlxGradient.createGradientFlxSprite(FlxG.width, Std.int(FlxG.height * 0.5), ([0xFF030007, 0xFFFFFFFF]));
+		//gradient2.alpha = 0.15;
+		gradient2.alpha = 0.15;
+		gradient2.blend = ADD;
+		gradient2.y = FlxG.height - gradient2.height;
+		add(gradient2);
+
+		warningText = new Alphabet(0, 80, 'WARNING!', true);
+		warningText.screenCenter(X);
+		warningText.color = 0xFFFF877E;
+		add(warningText);
+
+		var text:String = 'This mod contains shaders and flashing lights.';
+		infoText = new FlxText(0, warningText.y + warningText.height + 10, FlxG.width, text);
+		infoText.setFormat(Paths.font("FredokaOne-Regular.ttf"), 30, 0xFFF1E7FF, CENTER);
+		add(infoText);
+
+		pressEnterToContinueText = new Alphabet(0, 0, 'Press ENTER to continue!', false);
+		pressEnterToContinueText.setScale(0.8, 0.8);
+		pressEnterToContinueText.y = FlxG.height - pressEnterToContinueText.height - 50;
+		//pressEnterToContinueText.x += (128 * i) - 80;
+        pressEnterToContinueText.screenCenter(X);
+		pressEnterToContinueText.y += 10;
+		pressEnterToContinueText.alpha = 0;
+		pressEnterToContinueText.antialiasing = ClientPrefs.data.antialiasing;
+		add(pressEnterToContinueText);
+
+		FlxTween.tween(pressEnterToContinueText, {alpha: 1, y: pressEnterToContinueText.y - 10}, 0.2);
 	}
 
 	override function update(elapsed:Float)
@@ -64,8 +98,8 @@ class PerformanceWarning extends MusicBeatState
 			FlxTransitionableState.skipNextTransOut = true;
 			FlxG.sound.play(Paths.sound('confirmMenu'));
 
-			FlxTween.tween(warnText, {alpha: 0, y: warnText.y - 10}, 0.2);
-			FlxTween.tween(iUnderstand, {alpha: 0, y: iUnderstand.y - 10}, 0.2);
+			//FlxTween.tween(warnText, {alpha: 0, y: warnText.y - 10}, 0.2);
+			FlxTween.tween(pressEnterToContinueText, {alpha: 0, y: pressEnterToContinueText.y - 10}, 0.2);
 
 			new FlxTimer().start(1, function(t:FlxTimer)
 			{
