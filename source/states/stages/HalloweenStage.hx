867e6abc7c4040424a8f54a6f349acff47b123db
MrMadera <90923803+MrMadera@users.noreply.github.com>
2026-02-24 21:13:11 +0000

    week 2 optimization

--- source/states/stages/HalloweenStage.hx
+++ source/states/stages/HalloweenStage.hx
@@ -6,20 +6,20 @@ class HalloweenStage extends BaseStage
 	{
 		if(!ClientPrefs.data.lowQuality)
 		{
-                        var sky:BGSprite = new BGSprite('stages/halloweenStage/sky', -1248, -1041, 1, 1);
+                        var sky:BGSprite = new BGSprite('stages/halloweenStage/sky', -1248 + 250, -1041, 0, 0);
                         add(sky);
 
-                        var moon:BGSprite = new BGSprite('stages/halloweenStage/moon', -1315, -995, 0.1, 0.1);
+                        var moon:BGSprite = new BGSprite('stages/halloweenStage/moon', 485, -805, 0.1, 0.1);
                         add(moon);
 
                         var clouds:BGSprite = new BGSprite('stages/halloweenStage/clouds', -1286, -1146, 0.2, 0.2);
                         add(clouds);
                 }
                 
-                var buildings:BGSprite = new BGSprite('stages/halloweenStage/buildings', -1288, -1235, 0.6, 0.6);
+                var buildings:BGSprite = new BGSprite('stages/halloweenStage/buildings', -1038, -885, 0.6, 0.6);
                 add(buildings);
 
-                var bgmain:BGSprite = new BGSprite('stages/halloweenStage/bgmain', -1230, -1378, 1, 1);
+                var bgmain:BGSprite = new BGSprite('stages/halloweenStage/bgmain', -730, -878, 1, 1);
                 add(bgmain);
 
 		if(!ClientPrefs.data.lowQuality)
