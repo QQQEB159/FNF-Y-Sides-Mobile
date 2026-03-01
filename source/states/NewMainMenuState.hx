766a7a22cb13c2352b8643b8ed1e8c26671ff38f
MrMadera <90923803+MrMadera@users.noreply.github.com>
2026-03-01 10:53:43 +0000

    hehe

---
+++ source/states/NewMainMenuState.hx
@@ -0,0 +1,11 @@
+package states;
+
+class NewMainMenuState extends MusicBeatState 
+{
+    override function create()
+    {
+        super.create();
+        // coding at the library, but it's break time! :)
+        // hang on pls !!!
+    }
+}
