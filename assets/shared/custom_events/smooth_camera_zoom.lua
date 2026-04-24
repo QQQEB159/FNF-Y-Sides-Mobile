388322258c0c3ec2ae032dee0b84d3e25bf20b88
MrMadera <90923803+MrMadera@users.noreply.github.com>
2026-04-24 16:19:21 +0100

    hehe surpriese

--- assets/shared/custom_events/smooth_camera_zoom.lua
+++ assets/shared/custom_events/smooth_camera_zoom.lua
@@ -1,7 +1,12 @@
 function onEvent(name, value1, value2, strumTime)
     if name == 'smooth_camera_zoom' then
         local value_2 = stringSplit(value2, ",")
-        doTweenZoom('zoomTween', 'game', value1, value_2[1], value_2[2]);
+        if value_2[2] == 'instant' then
+            setProperty('camGame.zoom', value1)
+        else
+            doTweenZoom('zoomTween', 'game', value1, value_2[1], value_2[2]);
+        end
+            
         setProperty('defaultCamZoom', value1)
     end
 end
