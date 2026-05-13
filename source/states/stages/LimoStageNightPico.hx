a0af8a51b5b9813184cc9a0a23c4afe294f7f585
MrMadera <90923803+MrMadera@users.noreply.github.com>
2026-05-13 19:45:15 +0100

    new cool water!

--- source/states/stages/LimoStageNightPico.hx
+++ source/states/stages/LimoStageNightPico.hx
@@ -12,6 +12,8 @@ class LimoStageNightPico extends BaseStage
 
     var skyNight:FlxSprite;
     var clouds:FlxBackdrop;
+    public var airplaneLeft:FlxSprite;
+    public var airplaneRight:FlxSprite;
     var buildingsBack2:FlxBackdrop;
     var buildingsBack:FlxBackdrop;
     var buildingsFront:FlxBackdrop;
@@ -38,6 +40,30 @@ class LimoStageNightPico extends BaseStage
             clouds.x = LimoStage.cloudsXPos;
             clouds.antialiasing = ClientPrefs.data.antialiasing;
             add(clouds);
+
+            airplaneLeft = new FlxSprite(skyNight.x + skyNight.width + 700, skyNight.y + 500 + FlxG.random.float(-5, 105));
+            airplaneLeft.scrollFactor.set(0.25, 0.25);
+            //airplaneLeft.loadGraphic(Paths.image('stages/limoStage/airplane'));
+            airplaneLeft.frames = Paths.getSparrowAtlas('stages/limoStage/airplane');
+            airplaneLeft.animation.addByPrefix('idle', 'idle', 2, true);
+            airplaneLeft.animation.play('idle', true);
+            airplaneLeft.antialiasing = ClientPrefs.data.antialiasing;
+            airplaneLeft.scale.set(0.67, 0.67);
+            airplaneLeft.flipX = true;
+            add(airplaneLeft);
+
+            airplaneRight = new FlxSprite(skyNight.x, skyNight.y + 520 + FlxG.random.float(-5, 105));
+            airplaneRight.scrollFactor.set(0.25, 0.25);
+            //airplaneRight.loadGraphic(Paths.image('stages/limoStage/airplane'));
+            airplaneRight.frames = Paths.getSparrowAtlas('stages/limoStage/airplane');
+            airplaneRight.animation.addByPrefix('idle', 'idle', 2, true);
+            airplaneRight.animation.play('idle', true);
+            airplaneRight.antialiasing = ClientPrefs.data.antialiasing;
+            airplaneRight.scale.set(0.75, 0.75);
+            add(airplaneRight);
+
+            FlxTween.tween(airplaneLeft, {y: airplaneLeft.y + 10}, 3.5, {ease: FlxEase.sineInOut, type: PINGPONG});
+            FlxTween.tween(airplaneRight, {y: airplaneRight.y + 10}, 3.5, {ease: FlxEase.sineInOut, type: PINGPONG});
         }
 
         buildingsBack2 = new FlxBackdrop(Paths.image('stages/limoStage/night/buildingBack2'), X, 0, 0);
@@ -62,7 +88,7 @@ class LimoStageNightPico extends BaseStage
         add(buildingsFront);
 
         water = new FlxBackdrop(Paths.image('stages/limoStage/night/water'), X, 0, 0);
-        water.y = 560;
+        water.y = 990;
         water.x = -600;
         water.scrollFactor.set(0.65, 0.65);
         water.antialiasing = ClientPrefs.data.antialiasing;
@@ -100,28 +126,54 @@ class LimoStageNightPico extends BaseStage
     //var speedMult:Float = 10;
     function applyVelocites(isMaximumSpeed:Bool = false)
     {
-        if(clouds != null) clouds.velocity.set(targetSpeed, 0);
+        if(!ClientPrefs.data.lowQuality) clouds.velocity.set(targetSpeed, 0);
+        if(!ClientPrefs.data.lowQuality) airplaneRight.velocity.set(130, 0);
+        if(!ClientPrefs.data.lowQuality) airplaneLeft.velocity.set(-115, 0);
         buildingsBack2.velocity.set(targetSpeed * 2, 0); // 2x faster
         buildingsBack.velocity.set(targetSpeed * 2.5, 0); // 2.5 faster
         buildingsFront.velocity.set(targetSpeed * 3.75, 0); // 3.75 faster
+        water.velocity.set(targetSpeed * 3.75, 0); // 3.75 faster
         road.velocity.set(targetSpeed * 150, 0); // 150x faster
-        if(roadLights != null) roadLights.velocity.set(targetSpeed * 150, 0); // 150x faster
+        if(!ClientPrefs.data.lowQuality) roadLights.velocity.set(targetSpeed * 150, 0); // 150x faster
         // wind is 250x faster lmao
 
         if(isMaximumSpeed)
         {
-            if(clouds != null) clouds.velocity.set(targetSpeed * speedMult, 0);
+            if(!ClientPrefs.data.lowQuality) clouds.velocity.set(targetSpeed * speedMult, 0);
+            if(!ClientPrefs.data.lowQuality) airplaneRight.velocity.set(130 * speedMult, 0);
+            if(!ClientPrefs.data.lowQuality) airplaneLeft.velocity.set(-115 * speedMult, 0);
             buildingsBack2.velocity.set(targetSpeed * 2 * speedMult, 0);
             buildingsBack.velocity.set(targetSpeed * 2.5 * speedMult, 0);
             buildingsFront.velocity.set(targetSpeed * 3.75 * speedMult, 0);
+            water.velocity.set(targetSpeed * 3.75 * speedMult, 0);
             road.velocity.set(targetSpeed * 150 * speedMult, 0);
-            if(roadLights != null) roadLights.velocity.set(targetSpeed * 150 * speedMult, 0);
+            if(!ClientPrefs.data.lowQuality) roadLights.velocity.set(targetSpeed * 150 * speedMult, 0);
         }
     }
 
     override function update(elapsed:Float)
     {
         if(ClientPrefs.data.shaders) waterShader.iTime.value[0] += elapsed;
+        if(!ClientPrefs.data.lowQuality) 
+        {
+            if(airplaneRight.x > skyNight.x + 3660) 
+            {
+                airplaneRight.y = skyNight.y + 520 + FlxG.random.float(-5, 105);
+                airplaneRight.x = skyNight.x - 3600;
+
+                FlxTween.cancelTweensOf(airplaneRight);
+                FlxTween.tween(airplaneRight, {y: airplaneRight.y + 10}, 3.5, {ease: FlxEase.sineInOut, type: PINGPONG});
+            }
+
+            if(airplaneLeft.x < skyNight.x - 3660) 
+            {
+                airplaneLeft.y = skyNight.y + 500 + FlxG.random.float(-5, 105);
+                airplaneLeft.x = skyNight.x + skyNight.width + 300;
+
+                FlxTween.cancelTweensOf(airplaneLeft);
+                FlxTween.tween(airplaneLeft, {y: airplaneLeft.y + 10}, 3.5, {ease: FlxEase.sineInOut, type: PINGPONG});
+            }
+        }
     }
 
     override function stepHit()
